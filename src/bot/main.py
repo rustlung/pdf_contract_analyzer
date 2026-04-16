e3e10c07f797152ecd8b716494a2266242954e5a
Rustlung <v.i.sigaev@gmail.com>
2026-04-16 13:24:48 +0300

    fix: add proxy for telegram

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,7 +1,9 @@
 import asyncio
 import logging
+import os
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 from src.bot.config import get_bot_token
 from src.bot.handlers import setup_routers
 
@@ -10,7 +12,15 @@ async def main() -> None:
     logging.basicConfig(level=logging.INFO)
 
     token = get_bot_token()
-    bot = Bot(token=token)
+
+    proxy_url = os.getenv("HTTPS_PROXY") or os.getenv("HTTP_PROXY")
+    if proxy_url:
+        logging.info("Telegram bot using proxy: %s", proxy_url.split("@")[-1])
+        session = AiohttpSession(proxy=proxy_url)
+        bot = Bot(token=token, session=session)
+    else:
+        bot = Bot(token=token)
+
     dp = Dispatcher()
     dp.include_router(setup_routers())
 
